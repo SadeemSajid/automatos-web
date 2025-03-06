7508fb676ceccb26102000a1b717a368bd746611
Sadeem Sajid <sadeemsajid.25@gmail.com>
2025-03-06 23:58:10 +0500

    feat: redesign

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,10 +4,11 @@
 	}
 </script>
 
-<header class="absolute flex w-full items-center justify-between py-8">
-	<img src="logo-white.png" class="ml-44 w-12" alt="Automatos Logo" />
-	<div class="mr-44 flex gap-8">
-		<a href="#home" on:click={() => scroll('home')}><p>Home</p></a>
-		<a href="#home" on:click={() => scroll('middle')}><p>Services</p></a>
+<header class="mt-8 flex w-full flex-col items-center justify-between py-8">
+	<h2 class="mb-4 text-xl font-bold">AUTOMATOS.</h2>
+	<div class="flex gap-8">
+		<a href="#home" on:click={() => scroll('home')}><p>Services</p></a>
+		<a href="#home" on:click={() => scroll('middle')}><p>Products</p></a>
+		<a href="#home" on:click={() => scroll('middle')}><p>Portfolio</p></a>
 	</div>
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,32 +2,48 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 </script>
 
-<div class="flex flex-col">
+<div class="content-margin flex flex-col">
 	<!-- Main Section -->
-	<img id="home" class="h-screen object-cover" src="bg.png" alt="Website Background" />
-	<div class="absolute left-0 top-0 mt-60 flex h-full w-full flex-col items-center gap-4">
-		<!-- Your Overlay Content -->
-		<h1 class="font-light">Do <span class="font-bold">more</span> with less.</h1>
-		<h1 class="font-light">Work on what <span class="font-bold">matters</span>,</h1>
-		<h1 class="font-light">Let <span class="font-bold">us</span> tackle the problems.</h1>
+	<div class="mt-48 flex h-full w-full">
+		<!-- Text -->
+		<div class="mx-48 flex flex-col gap-2">
+			<h1 class="font-normal">Do <span class="text-pink font-bold">more</span> with less.</h1>
+			<h1 class="font-normal">
+				Work on what <span class="text-pink font-bold">matters</span>, let
+			</h1>
+			<h1 class="font-normal">
+				<span class="text-pink font-bold">us</span> tackle the problems.
+			</h1>
+			<Button
+				class="font-josefin mt-12 w-56 rounded-md border-2 border-white bg-transparent py-7 text-2xl text-white hover:text-black"
+				>Let’s Talk :D</Button
+			>
+		</div>
 
-		<Button
-			class="mt-12 bg-white/20 p-8 text-3xl text-white shadow-lg backdrop-blur-md hover:text-black"
-			>Let’s Talk 🥳</Button
-		>
+		<!-- Image -->
+		<img
+			src="logo-light-outline.png"
+			class=" ml-[-320px] mt-[-80px] h-fit w-64 opacity-20"
+			alt="Automatos Logo Outline"
+		/>
 	</div>
 
 	<!-- Rest of the page -->
-	<div
-		id="middle"
-		class="h-screen bg-gradient-to-br from-[#0F0917] via-[#170417] to-[#170417] px-44 pt-12"
-	>
-		<h3 class="">We do a lot of things.</h3>
-		<p class="mr-96 mt-8 text-3xl">
-			We’re an awesome team turning ideas into powerful digital solutions. Need a sleek web app, a
-			rock-solid mobile app, or AI that actually works? We build it smart, scalable, and
-			future-ready—so your business stays ahead, and we remain curious.
-		</p>
+	<div id="middle" class="mt-32 h-screen pt-64">
+		<div class="flex items-center justify-center">
+			<!-- Text -->
+			<div class="flex w-[900px] flex-col">
+				<h1 class="">We do a lot of things.</h1>
+				<p class="mr-52 mt-8 text-3xl">
+					We’re an awesome team turning ideas into powerful digital solutions. Need a sleek web app,
+					a rock-solid mobile app, or AI that actually works? We build it smart, scalable, and
+					future-ready—so your business stays ahead, and we remain curious.
+				</p>
+			</div>
+
+			<!-- Graphic -->
+			<img src="green-diamonds.png" class="w-96" alt="" />
+		</div>
 
 		<!-- TODO -->
 		<!-- 3 columns -->
