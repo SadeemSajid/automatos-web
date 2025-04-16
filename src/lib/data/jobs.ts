63271b663dd58715094c0ce6656b41ff7e9ef890
Sadeem Sajid <sadeemsajid.25@gmail.com>
2025-04-17 01:02:28 +0500

    chore: job form

--- src/lib/data/jobs.ts
+++ src/lib/data/jobs.ts
@@ -4,7 +4,7 @@ export const jobs = [
 		title: 'Software Engineer Intern',
 		location: 'Remote',
 		datePosted: '2025-04-16',
-		link: 'https://www.linkedin.com/jobs/view/4208312566',
+		link: 'https://forms.gle/L7UDAWWxbX2Q2jpV9',
 		description: `This is not just an internship, it’s a chance to enter the engine room of modern innovation. You'll be trusted with real code, real problems, and real ownership.`
 	}
 ];
