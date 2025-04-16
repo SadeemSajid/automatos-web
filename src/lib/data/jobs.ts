5903115982b70d74fd8ab381a52a6510bcfece66
Sadeem Sajid <sadeemsajid.25@gmail.com>
2025-04-17 01:11:56 +0500

    chore: job update

--- src/lib/data/jobs.ts
+++ src/lib/data/jobs.ts
@@ -6,5 +6,13 @@ export const jobs = [
 		datePosted: '2025-04-16',
 		link: 'https://forms.gle/L7UDAWWxbX2Q2jpV9',
 		description: `This is not just an internship, it’s a chance to enter the engine room of modern innovation. You'll be trusted with real code, real problems, and real ownership.`
+	},
+	{
+		id: 2,
+		title: 'Business Development Associate',
+		location: 'Remote',
+		datePosted: '2025-04-16',
+		link: 'https://forms.gle/5Y9WcBa91UjFd8qEA',
+		description: `The Business Developer will assist in generating leads, conducting market research, and analyzing data.`
 	}
 ];
